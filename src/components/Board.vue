5b5dacd600db152c49c939a488cf022aa85019d7
Sil van Diepen <me@sil.mt>
2025-03-18 16:56:52 +0100

    feat: Create Board component for displaying cards

---
+++ src/components/Board.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import { useBemm } from "bemm";
+
+const bemm = useBemm('board')
+
+</script>
+
+<template>
+  <div :class="bemm()">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.board {
+  display: flex;
+  flex-wrap: wrap;
+align-items: center;
+  justify-content: center;
+  gap: var(--space);
+}
+</style>
